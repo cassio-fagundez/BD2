6adab70460d78286579839bdd1e5a5e0c7cf1aaf
cassio-fagundez <ca.yo.fa.vi1@gmail.com>
2021-03-23 20:29:28 -0300

    Excercise 3 fixed.

--- exercicios-3/3.sql
+++ exercicios-3/3.sql
@@ -3,5 +3,5 @@ use world;
 /* 3. Retorne o nome dos continentes e o total de países que neles existem. Ordene o resultado em
 ordem alfabética pelo nome do continente.  */
 
-SELECT c.Continent AS Nome_Continentes, COUNT(c.Continent) AS Total_Países FROM  country c
-GROUP BY c.Continent ORDER BY c.Continent;
+SELECT CAST(c.Continent AS CHAR) AS Nome_Continentes, COUNT(c.Continent) AS Total_Países FROM  country c
+GROUP BY c.Continent ORDER BY 1;
